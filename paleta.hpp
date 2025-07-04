014d210acf904bdccd605beb98c239b14977a5f7
julioinrainbows <juliocesarsouaz@gmail.com>
2025-07-04 10:32:56 -0300

    Update paleta.hpp

--- paleta.hpp
+++ paleta.hpp
@@ -1,12 +1,11 @@
 #ifndef PALETA_HPP
 #define PALETA_HPP
 #include "cor.hpp"
-
+#include <vector>
 class Paleta {
     public:
         int quantidade;
-        Cor Cores[100];
-        int valores[100];//100 cores com 100 valores de referencia;
-
+        std::vector<Cor> Cores;
+        std::vector<int> Valores;//100 cores com 100 valores de referencia;
 };
 #endif
