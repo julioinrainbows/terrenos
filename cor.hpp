7742d12469333d8acb370983c7dddbf7d9f3b28c
julioinrainbows <juliocesarsouaz@gmail.com>
2025-07-03 23:16:56 -0300

    Etapa 1 em progresso

---
+++ cor.hpp
@@ -0,0 +1,11 @@
+#ifndef COR_HPP
+#define COR_HPP
+
+class Cor {
+public:
+    int R, G, B;    
+    Cor();
+    Cor(int r, int g, int b);
+};
+
+#endif
---
+++ paleta.hpp
@@ -0,0 +1,12 @@
+#ifndef PALETA_HPP
+#define PALETA_HPP
+#include "cor.hpp"
+
+class Paleta {
+    public:
+        int quantidade;
+        Cor Cores[100];
+        int valores[100];//100 cores com 100 valores de referencia;
+
+};
+#endif
