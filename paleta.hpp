71a7f567095148b5641ade1cb85111cece2008f5
julioinrainbows <juliocesarsouaz@gmail.com>
2025-07-08 17:19:58 -0300

    Update paleta.hpp

--- paleta.hpp
+++ paleta.hpp
@@ -1,4 +1,5 @@
-#include <iostream>
+#ifndef PALETA_HPP
+#define PALETA_HPP
 #include <vector>
 #include <fstream>
 #include <string>
@@ -15,32 +16,6 @@ class Paleta {
         std::vector<Cor> Cores;
         std::vector<int> Valores; //100 cores com 100 valores de referencia;
         
-        void definevalues() {
-            ifstream text ("Paleta.txt");
-            string str;
-            getline(text, str);
-            int quantint = stoi(str);
-    
-            Paleta P1;
-            P1.quantidade = quantint;
-            P1.Cores.resize(quantint);
-            P1.Valores.resize(quantint);
-
-            int i = 0;
-            while (getline(text, str)) {
-                stringstream ss(str);
-                vector<string> v;
-                string temp;
-
-                while(getline(ss, temp, ' ')) {
-                    v.push_back(temp);
-                }
-
-                P1.Valores[i] = stoi(v[0]);
-                P1.Cores[i].R = stoi(v[1]);
-                P1.Cores[i].G = stoi(v[2]);
-                P1.Cores[i].B = stoi(v[3]);
-                i++;
-            }
-        }
-};
+        void definevalues(std::string arquivo);
+}; //a
+#endif
