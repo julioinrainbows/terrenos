b33242254f0bce2ec9d9e698610840741f31bc65
GuiChavesUFRN <guilherme.chaves.139@ufrn.edu.br>
2025-07-09 17:03:30 -0300

    Update paleta.cpp

--- paleta.cpp
+++ paleta.cpp
@@ -4,27 +4,27 @@
 #include <string>
 using namespace std;
 void Paleta::definevalues(std::string arquivo){
-         ifstream text(arquivo);
-            string str;
-            getline(text, str);
-            int quantint = stoi(str);
-            quantidade = quantint;
-            Cores.resize(quantint);
-            Valores.resize(quantint);
-            int i = 0;
-            while (getline(text, str)) {
-                stringstream ss(str);
-                vector<string> v;
-                string temp;
+    ifstream text(arquivo);
+    string str;
+    getline(text, str);
+    int quantint = stoi(str);
+    quantidade = quantint;
+    Cores.resize(quantint);
+    Valores.resize(quantint);
+    int i = 0;
+    while (getline(text, str)) {
+        stringstream ss(str);
+        vector<string> v;
+        string temp;
 
-                while(getline(ss, temp, ' ')) {
-                    v.push_back(temp);
-                }
+        while(getline(ss, temp, ' ')) {
+            v.push_back(temp);
+        }
 
-                Valores[i] = stoi(v[0]);
-                Cores[i].R = stoi(v[1]);
-                Cores[i].G = stoi(v[2]);
-                Cores[i].B = stoi(v[3]);
-                i++;
-            }
-        }//b
+        Valores[i] = stoi(v[0]);
+        Cores[i].R = stoi(v[1]);
+        Cores[i].G = stoi(v[2]);
+        Cores[i].B = stoi(v[3]);
+        i++;
+    }
+}//b
