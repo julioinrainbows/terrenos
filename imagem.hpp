a9939b08c3118d5971f71e18d9fa16db69fcedd7
GuiChavesUFRN <guilherme.chaves.139@ufrn.edu.br>
2025-07-10 10:28:19 -0300

    Update imagem.hpp

--- imagem.hpp
+++ imagem.hpp
@@ -12,7 +12,7 @@ class imagem {
 
     public:
         imagem(int a, int l);
-        pixel consultar(int x, int y, imagem img);
-        void colorir(int x, int y, imagem &img, short R, short G, short B);
-        void criarPPM(string arquivo, imagem &img, int a, int l);
+        pixel consultar(int x, int y);
+        void colorir(int x, int y, short R, short G, short B);
+        void criarPPM(string arquivo);
 };
