0df247db2bd1c356055e4e1ff8d4dd674da55ab2
GuiChavesUFRN <guilherme.chaves.139@ufrn.edu.br>
2025-07-11 11:42:36 -0300

    Update mapa.cpp

--- mapa.cpp
+++ mapa.cpp
@@ -6,28 +6,32 @@
     #include "mapa.hpp"
     using namespace std;
 
-    terreno::terreno(int a, int l): altura(a), largura(l) {
+    terreno::terreno(int n): dimensao(n) {
         srand(time(0));
-        mapa.resize(altura, vector<int>(largura,0));
-        for(int i = 0; i < altura; i++) {
-            for(int j = 0; j < largura; j++) {
+        mapa.resize(n, vector<int>(n,0));
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
                 mapa[i][j] = 0;
-                }
             }
+        }
     }
     //s d s d
     void terreno::gerarTerreno(int n, float rug){
-        int tamanho = static_cast<int>(pow(2,n)+1);
+        long long tamanho = static_cast<long long>(pow(2,n)+1);
+        cout << tamanho << endl;
         srand(time(0));
-        std::vector<vector<int>> mapaalt;
+        std::vector<vector<long long>> mapaalt;
+
         mapa.resize(tamanho,vector<int>(tamanho));
         mapa[0][0] = rand() % 100;
-        mapa[0][tamanho-1] = rand() % 100;
-        mapa[tamanho-1][0] = rand() % 100;
-        mapa[tamanho-1][tamanho-1] = rand() % 100;
-        int step = tamanho - 1;   
+        mapa[0][n-1] = rand() % 100;
+        mapa[n-1][0] = rand() % 100;
+        mapa[n-1][n-1] = rand() % 100;
+
+        int step = tamanho - 1;
         while(step > 1){
             int half = step/2;
+
             for(int x = half; x < tamanho-1; x += step){ //diamond step.
                 for(int y = half; y < tamanho-1; y+= step){
                     int media = (mapa[x-half][y-half] + mapa[x-half][y+half] + mapa[x+half][y-half] + mapa[x+half][y+half])/4;
@@ -35,6 +39,7 @@
                     mapa[x][y] = media + random;
                 }
             }
+
             for (int x = 0; x < tamanho; x += half) { // squarestep
                 for (int y = (x + half) % step; y < tamanho; y += step) {
                     int soma = 0;
@@ -48,10 +53,11 @@
                     mapa[x][y] = static_cast<int>(media + random);
                 }
             }
-        rug/2;
-        step /=2;
+
+        rug /= 2;
+        step /= 2;
+        }
     }
-}
 
     int terreno::getAltitude(int x, int y){
     return mapa[x][y];
